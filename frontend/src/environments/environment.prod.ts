fe425e14b7c705e1c7b059b64a2947a086af09d2
Miguel Largo <largoamado@gmail.com>
2025-11-07 16:01:38 -0500

    Fix: agregar prefijo /api a la URL del backend en environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://softone-backend-useast1.eba-epvnmbmk.us-east-1.elasticbeanstalk.com',
+    apiUrl: 'http://softone-backend-useast1.eba-epvnmbmk.us-east-1.elasticbeanstalk.com/api',
     openaiApiKey: ''
 };
